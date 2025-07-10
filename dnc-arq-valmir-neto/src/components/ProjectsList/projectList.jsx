f322bdb4f61196f23f605ba766754fd5f999b4f3
Valmir-Neto <netovalmirr@gmail.com>
2025-07-10 18:20:08 -0300

    estrutura do componente projectList pronta

--- dnc-arq-valmir-neto/src/components/ProjectsList/projectList.jsx
+++ dnc-arq-valmir-neto/src/components/ProjectsList/projectList.jsx
@@ -4,12 +4,18 @@ import likeFill from '../../assets/like-filled.svg'
 import like from '../../assets/like.svg'
 
 function ProjectList() {
-    return(
+    return (
         <div className="container">
             <div className="project-grid">
+                <h2>Follow Our Projects</h2>
+                <p>It is a long established fact that a reader will be distracted by the of readable <br/> content of page  lookings at its layouts  points.</p>
                 <div className="card">
-                    <div className="bg"></div>
-
+                   <div className="exemple-poster"></div>
+                    <h3>nome ficticio</h3>
+                    <p>Cidade ficticia</p>
+                    <button aria-label='liked button'>
+                        <img src={like} alt='like button' />
+                    </button>
                 </div>
             </div>
         </div>
--- dnc-arq-valmir-neto/src/pages/home.jsx
+++ dnc-arq-valmir-neto/src/pages/home.jsx
@@ -9,7 +9,7 @@ function Home() {
         <>
         <Header />
         <Hero />
-        <ProjectList />
+        <ProjectList/>
         <h1>Home</h1>
         <Footer />
         </>

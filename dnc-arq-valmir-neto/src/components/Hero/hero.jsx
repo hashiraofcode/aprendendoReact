3ef030520cf95c843838f7402a67a04fe5ed2cb1
Valmir-Neto <netovalmirr@gmail.com>
2025-07-10 15:40:24 -0300

    finalizando o componente hero

--- dnc-arq-valmir-neto/src/components/Hero/hero.jsx
+++ dnc-arq-valmir-neto/src/components/Hero/hero.jsx
@@ -1,14 +1,20 @@
 import '../Hero/hero.css'
 import Button from '../buttonsAplication/button'
+import backgroundImage from "../../assets/bg-hero-image.svg"
+import {Link} from 'react-router-dom'
+
+
 function Hero() {
     return (
         <>
             <div className="container">
-                <div className="hero">
-                    <div className="heroContent">
-                        <h2>Let Your Home <br /> Be Unique</h2>
-                        <p>There are many variations of the passages of <br/> lorem Ipsum fromavailable, majority.</p>
-                        <Button StyleButton={'secundary'} arrow>Get started</Button>
+                <div className="hero flex-container flex-align-center" style={{backgroundImage:`url(${backgroundImage})`}}>
+                    <div className="heroContent flex-container flex-direction">
+                        <h2 className='text-secundary font-title-oversize'>Let Your Home <br /> Be Unique</h2>
+                        <p className='font-text'>There are many variations of the passages of <br/> lorem Ipsum fromavailable, majority.</p>
+                        <Link to='/about'>
+                         <Button StyleButton={'secundary'} arrow>Get started</Button>
+                        </Link>
                     </div>
                 </div>
             </div>

7e063058f0c599548440d1c8b33b0d2488904732
Valmir-Neto <netovalmirr@gmail.com>
2025-07-10 16:58:40 -0300

    iniciando a criação do project List

---
+++ dnc-arq-valmir-neto/src/components/ProjectsList/projectList.jsx
@@ -0,0 +1,16 @@
+import '../ProjectsList/projectList.css'
+
+function ProjectList() {
+    return(
+        <div className="container">
+            <div className="project-grid">
+                <div className="card">
+                    <div className="bg"></div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectList
--- dnc-arq-valmir-neto/src/pages/home.jsx
+++ dnc-arq-valmir-neto/src/pages/home.jsx
@@ -1,12 +1,15 @@
+// Components
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/footer'
-import Buttons from '../components/buttonsAplication/button'
 import Hero from '../components/Hero/hero'
+import ProjectList from '../components/ProjectsList/projectList'
+
 function Home() {
     return(
         <>
         <Header />
         <Hero />
+        <ProjectList />
         <h1>Home</h1>
         <Footer />
         </>

b35fcbb500adc21a4b39bc32d98d3349403d26b0
Valmir-Neto <netovalmirr@gmail.com>
2025-07-11 14:52:25 -0300

    Terminando component project list

--- dnc-arq-valmir-neto/src/components/ProjectsList/projectList.jsx
+++ dnc-arq-valmir-neto/src/components/ProjectsList/projectList.jsx
@@ -6,16 +6,106 @@ import like from '../../assets/like.svg'
 function ProjectList() {
     return (
         <div className="container">
-            <div className="project-grid">
-                <h2>Follow Our Projects</h2>
-                <p>It is a long established fact that a reader will be distracted by the of readable <br/> content of page  lookings at its layouts  points.</p>
-                <div className="card">
-                   <div className="exemple-poster"></div>
-                    <h3>nome ficticio</h3>
-                    <p>Cidade ficticia</p>
-                    <button aria-label='liked button'>
-                        <img src={like} alt='like button' />
-                    </button>
+            <div className="section-projects flex-container jc-center">
+                <h2 className='font-other-titles align-center'>Follow Our Projects</h2>
+                <p className='font-text align-center'>It is a long established fact that a reader will be distracted by the of readable <br /> content of page  lookings at its layouts  points.</p>
+                <div className="project-grid flex-container jc-center flex-align-center flex-wrap">
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
+                    <div className="card flex-container flex-direction">
+                        <div className="exemple-poster"></div>
+                        <h3 className='small-titles'>nome ficticio</h3>
+                        <p className='font-text'>Cidade ficticia</p>
+                        <button aria-label='liked button'>
+                            <img src={like} alt='like button' />
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>

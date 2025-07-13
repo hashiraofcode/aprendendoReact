eff13e714ee721f555d6d82048668de019b0bdcd
Valmir-Neto <netovalmirr@gmail.com>
2025-07-13 13:40:07 -0300

    estilizando area do formulário

--- dnc-arq-valmir-neto/src/components/ContactForm/contactform.jsx
+++ dnc-arq-valmir-neto/src/components/ContactForm/contactform.jsx
@@ -1,6 +1,8 @@
+import '../ContactForm/contactform.css'
 function ContactForm() {
-    return(
-        <div className="container">
+    return (
+        <div className="container contact-container flex-container jc-center">
+            <h2 className='contact-title flex-container jc-center font-other-titles'>We love meeting new people<br/>and helping them.</h2>
             <form action=""></form>
         </div>
     )
--- dnc-arq-valmir-neto/src/pages/contacts.jsx
+++ dnc-arq-valmir-neto/src/pages/contacts.jsx
@@ -1,17 +1,16 @@
-import Banner from '../components/Banner/banner.jsx' 
+import Banner from '../components/Banner/banner.jsx'
 import Header from '../components/Header/Header.jsx'
 import ContactForm from '../components/ContactForm/contactform.jsx'
 import Footer from '../components/Footer/footer.jsx'
 import ImageBanner from '../assets/bg-contact-image.svg'
 
 function Contacts() {
-    return(
+    return (
         <>
-        <Header/>
-        <Banner image={ImageBanner} title='Contact'/>
-        <h1>contato</h1>
-        <ContactForm/>
-        <Footer />
+            <Header />
+            <Banner image={ImageBanner} title='Contact' />
+            <ContactForm />
+            <Footer />
         </>
     )
 }

043e7c4c10b34ee4ec6d49ee5333626ab60d46d7
Valmir-Neto <netovalmirr@gmail.com>
2025-07-16 17:46:17 -0300

    corrigindo código da função que consome api

--- dnc-arq-valmir-neto/src/services/apiService.js
+++ dnc-arq-valmir-neto/src/services/apiService.js
@@ -1,12 +1,11 @@
  export const apiService = async (endingpoint, parms) => {
     try {
         const URL = new URL (`https://react.dnc.group/files/${endingpoint}`);
-        const apiData = fetch(URL);
-        const response = await apiData;
+         const response = await fetch(URL);
         if(!response.ok) {
             throw new Error(`houve um erro  ${response.status}: ${response.statusText}`);
         }
-        const data = await response.json;
+        const data = await response.json();
         return data
     } catch (err) {
         console.error(err);

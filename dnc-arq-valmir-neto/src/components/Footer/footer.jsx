32c76259065e76364a01a90f833a1f9b0f605497
Valmir-Neto <netovalmirr@gmail.com>
2025-07-21 15:36:19 -0300

    ajustando o footer com a função callBack

--- dnc-arq-valmir-neto/src/components/Footer/footer.jsx
+++ dnc-arq-valmir-neto/src/components/Footer/footer.jsx
@@ -13,6 +13,8 @@ import UsaFlag from '../../assets/coutry-USA-Flag.svg'
 import { useContext } from 'react'
 import { AppContext } from '../../context/textsContext'
 
+// UTILITS
+import { callBackDefaultContent } from '../../utilits/callBackDefaultContent'
 
 
 
@@ -22,67 +24,71 @@ function Footer() {
     const language = dataContext?.language;
     const setLanguage = dataContext?.setLanguage;
 
-// função definir idiomas;
-const setLang = (language) => {
-    setLanguage(language)
-}
+    // função definir idiomas;
+    const setLang = (language) => {
+        setLanguage(language)
+    }
 
     return (
         <>
-        <div className="container">
-          <footer className='flex-container flex-align-center flex-wrap'>
-                <div className="content-footer flex-container flex-align-center flex-wrap">
-                         <div className='logo-socialMedia-container flex-container flex-direction flex-children max-width-30rem '>
-                    <img src={dncLogo} alt="Logo Dnc" />
-                    <p>{contentApplication[language]?.general.footerLogoText}</p>
-                    <div className="social-media flex-container">
-                        <img src={facebookLogo} alt="logo do facebook" aria-label='button social media' /><img src={twiterLogo} alt="logo do twiter" aria-label='button social media' /><img src={linkeldnLogo} alt="logo do linkeldn" aria-label='button social media' /><img src={instagramLogo} alt="logo do instagram" aria-label='button social media' />
+            <div className="container">
+                <footer className='flex-container flex-align-center flex-wrap'>
+                    <div className="content-footer flex-container flex-align-center flex-wrap">
+                        <div className='logo-socialMedia-container flex-container flex-direction flex-children max-width-30rem '>
+                            <img src={dncLogo} alt="Logo Dnc" />
+                            <p>{callBackDefaultContent(contentApplication, language, 'general', 'footerLogoText')}</p>
+                            <div className="social-media flex-container">
+                                <img src={facebookLogo} alt="logo do facebook" aria-label='button social media' /><img src={twiterLogo} alt="logo do twiter" aria-label='button social media' /><img src={linkeldnLogo} alt="logo do linkeldn" aria-label='button social media' /><img src={instagramLogo} alt="logo do instagram" aria-label='button social media' />
+                            </div>
+                        </div>
+                        <div className='links-container flex-children flex-container flex-direction jc-center flex-wrap'>
+                            <h5 className='align-center trasform-text small-titles'>{callBackDefaultContent(contentApplication, language, 'general', 'pages')}</h5>
+                            <ul className='links-list flex-container flex-direction jc-center'>
+                                <li>
+                                    <Link to='/'>
+                                        {callBackDefaultContent(contentApplication, language, 'menu', 'home')}
+                                    </Link>
+                                </li>
+                                <li><Link to='/about'>{callBackDefaultContent(contentApplication, language, 'menu', 'about')}</Link></li>
+                                <li><Link to='/projects'>{callBackDefaultContent(contentApplication, language, 'menu', 'projects')}</Link></li>
+                                <li><Link to='/contact'>{callBackDefaultContent(contentApplication, language, 'menu', 'contact')}</Link></li>
+                            </ul>
+                        </div>
+                        <div className="adress-container flex-children max-width-20rem flex-container flex-direction jc-center">
+                            <h5 className=' trasform-text small-titles'></h5>
+                            <address className='flex-container flex-direction jc-center'>
+                                <p>
+                                    R. Justino Cobra, 61 – Vila Ema | São José dos Campos – SP | CEP 12243-030
+                                </p>
+                                <p>suporte@escoladnc.com.br</p>
+                                <p>(19) 99187-4342</p>
+                            </address>
+                        </div>
                     </div>
-                </div>
-                <div className='links-container flex-children flex-container flex-direction jc-center flex-wrap'>
-                    <h5 className='align-center trasform-text small-titles'>{contentApplication[language]?.general.pages}</h5>
-                    <ul className='links-list flex-container flex-direction jc-center'>
-                        <li><Link to='/'>{contentApplication[language]?.menu.home}</Link></li>
-                        <li><Link to='/about'>{contentApplication[language]?.menu.about}</Link></li>
-                        <li><Link to='/projects'>{contentApplication[language]?.menu.projects}</Link></li>
-                        <li><Link to='/contact'>{contentApplication[language]?.menu.contact}</Link></li>
-                    </ul>
-                </div>
-                <div className="adress-container flex-children max-width-20rem flex-container flex-direction jc-center">
-                    <h5 className=' trasform-text small-titles'>{contentApplication[language]?.general.contact}</h5>
-                    <address className='flex-container flex-direction jc-center'>
-                        <p>
-                            R. Justino Cobra, 61 – Vila Ema | São José dos Campos – SP | CEP 12243-030
-                        </p>
-                        <p>suporte@escoladnc.com.br</p>
-                        <p>(19) 99187-4342</p>
-                    </address>
-                </div>
-                </div>
-                <div className="footer-languages-credits flex-container jc-space-between flex-wrap">
-                    <p>Copyright  &copy; DNC - 2024 </p>
-                    <div className="coutry-flag">
-                        <img 
-                        src={brazilianFlag} 
-                        alt="brazilian" 
-                        aria-label='Select your language "Portugues Brazilian"' 
-                        onClick={() => {
-                            setLang('br');
-                        }}
-                        />
-                        <img 
-                        src={UsaFlag} 
-                        alt="United States" 
-                        aria-label='Select your language "Inglish"' 
-                        onClick={() => {
-                            setLang('en');
-                        }} 
-                        />
+                    <div className="footer-languages-credits flex-container jc-space-between flex-wrap">
+                        <p>Copyright  &copy; DNC - 2024 </p>
+                        <div className="coutry-flag">
+                            <img
+                                src={brazilianFlag}
+                                alt="brazilian"
+                                aria-label='Select your language "Portugues Brazilian"'
+                                onClick={() => {
+                                    setLang('br');
+                                }}
+                            />
+                            <img
+                                src={UsaFlag}
+                                alt="United States"
+                                aria-label='Select your language "Inglish"'
+                                onClick={() => {
+                                    setLang('en');
+                                }}
+                            />
+                        </div>
                     </div>
-                </div>
 
-            </footer>
-        </div>
+                </footer>
+            </div>
         </>
     )
 }
--- dnc-arq-valmir-neto/src/utilits/callBackDefaultContent.js
+++ dnc-arq-valmir-neto/src/utilits/callBackDefaultContent.js
@@ -1,3 +1,5 @@
 export const callBackDefaultContent = (data, language,path,propertie, fallback = 'carregando...') => {
     return data?.[language]?.[path]?.[propertie] ?? fallback
 }
+
+// função que garante que a aplicação não vai quebrar caso os dados assincronos ainda não estejam prontos;

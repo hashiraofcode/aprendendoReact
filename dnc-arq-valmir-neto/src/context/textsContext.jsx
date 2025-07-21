6e7f704dae9ecb7b2887515856714cd75eb99a7d
Valmir-Neto <netovalmirr@gmail.com>
2025-07-20 21:45:17 -0300

    api contenxt criada

--- dnc-arq-valmir-neto/src/context/textsContext.jsx
+++ dnc-arq-valmir-neto/src/context/textsContext.jsx
@@ -1,7 +1,35 @@
 import { createContext, useState, useEffect } from "react";
 
-export const appContext = createContext();
+export const AppContext = createContext();
 
-export const AppProvider = () => {
-    
+export const AppProvider = ({children}) => {
+    const [language, setLanguage] = useState('br');
+    const [contentApplication, setContentApplication] = useState();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(()=>{
+        const fetchTexts = async () => {
+            try {
+                const response = await fetch('/api/webText');
+                const data = await response.json();
+                if (!response.ok) {
+                    throw new Error(`Erro: ${response.status} = ${response.text}`);
+                }
+                setContentApplication(data);
+            } catch (e) {
+                console.error(e);
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchTexts();
+    },[])
+
+
+    return (
+        <AppContext.Provider value={{contentApplication, setLanguage, language, loading}}>
+            {children}
+        </AppContext.Provider>
+    )
 }

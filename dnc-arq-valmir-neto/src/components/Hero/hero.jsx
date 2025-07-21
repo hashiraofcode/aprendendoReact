93e4715b1e6795ba224b2a32f9cede0223e83926
Valmir-Neto <netovalmirr@gmail.com>
2025-07-21 15:54:08 -0300

    context hero concluido

--- dnc-arq-valmir-neto/src/components/Hero/hero.jsx
+++ dnc-arq-valmir-neto/src/components/Hero/hero.jsx
@@ -1,19 +1,28 @@
 import '../Hero/hero.css'
+import { useContext } from 'react'
 import Button from '../buttonsAplication/button'
 import backgroundImage from "../../assets/bg-hero-image.svg"
 import {Link} from 'react-router-dom'
 
+// CONTEXT
+import { AppContext } from '../../context/textsContext'
+
+// UTILITS
+import { callBackDefaultContent } from '../../utilits/callBackDefaultContent'
 
 function Hero() {
+    const dataContext = useContext(AppContext);
+    const contentApplication = dataContext?.contentApplication;
+    const language = dataContext?.language;
     return (
         <>
             <div className="container">
                 <div className="hero flex-container flex-align-center" style={{backgroundImage:`url(${backgroundImage})`}}>
                     <div className="heroContent flex-container flex-direction">
-                        <h2 className='text-secundary font-title-oversize'>Let Your Home <br /> Be Unique</h2>
-                        <p className='font-text'>There are many variations of the passages of <br/> lorem Ipsum fromavailable, majority.</p>
+                        <h2 className='text-secundary font-title-oversize'>{callBackDefaultContent(contentApplication,language,'hero','title')}</h2>
+                        <p className='font-text'>{callBackDefaultContent(contentApplication,language,'hero','subtitle')}</p>
                         <Link to='/about'>
-                         <Button StyleButton={'secundary'} arrow>Get started</Button>
+                         <Button StyleButton={'secundary'} arrow>{callBackDefaultContent(contentApplication,language,'hero','cta')}</Button>
                         </Link>
                     </div>
                 </div>

d953e4b110319113ff22d02eb15223e2067432f5
Valmir-Neto <netovalmirr@gmail.com>
2025-07-21 16:06:59 -0300

    context projects concluida

--- dnc-arq-valmir-neto/src/components/About/about.jsx
+++ dnc-arq-valmir-neto/src/components/About/about.jsx
@@ -1,14 +1,27 @@
 import '../About/about.css'
+import { useContext } from 'react';
+
+//CONTEXTO
+import { AppContext } from '../../context/textsContext';
+
+// UTILITS
+import { callBackDefaultContent } from '../../utilits/callBackDefaultContent';
+
 
 function About() {
+    const contextData = useContext(AppContext);
+    const contentApplication = contextData?.contentApplication;
+    const language = contextData?.language;
+
+
     return (
         <div className="container aboutContent flex-container">
-            <h3 className='title-about-content font-other-titles'>We set the trends of modern living  Services.</h3>
-                <div className="content-wrapper flex-container flex-direction">
-                    <p className='text-primary font-text'>It is a long established fact that a reader will be distracted by the of readable content of a page</p>
-                    <p className='text-tertiary font-text'>It is a long established fact that a reader will be distracted by the of readable content of a page</p>
-                    <p className='text-tertiary font-text'>It is a long established fact that a reader will be distracted by the of readable content of a page</p>
-                </div>
+            <h3 className='title-about-content font-other-titles'>{callBackDefaultContent(contentApplication, language, 'about','title')}</h3>
+            <div className="content-wrapper flex-container flex-direction">
+                <p className='text-primary font-text'>{callBackDefaultContent(contentApplication, language, 'about','p1')}</p>
+                <p className='text-tertiary font-text'>{callBackDefaultContent(contentApplication, language, 'about','p2')}</p>
+                <p className='text-tertiary font-text'>{callBackDefaultContent(contentApplication, language, 'about','p3')}</p>
+            </div>
         </div>
     )
 }

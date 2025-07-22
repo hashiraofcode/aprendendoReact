70a76c4e65034c4fab21f0212fb9b8c2ecba67bf
Valmir-Neto <netovalmirr@gmail.com>
2025-07-22 13:30:45 -0300

    salvando idioma escolhido pelo user no local Storage

--- dnc-arq-valmir-neto/src/context/textsContext.jsx
+++ dnc-arq-valmir-neto/src/context/textsContext.jsx
@@ -3,9 +3,11 @@ import { createContext, useState, useEffect } from "react";
 export const AppContext = createContext({});
 
 export const AppProvider = ({ children }) => {
-    const [language, setLanguage] = useState('br');
+    const savedLang = localStorage.getItem('lang');
+    const [language, setLanguage] = useState(savedLang ?? 'br');
     const [contentApplication, setContentApplication] = useState(null);
     const [loadingThisContent, setLoadingThisContent] = useState(true);
+    
 
     useEffect(() => {
         const fetchTexts = async () => {
@@ -22,10 +24,13 @@ export const AppProvider = ({ children }) => {
                 setLoadingThisContent(false);
             }
         }
-
+        
         fetchTexts();
     }, [])
 
+    useEffect(()=> { 
+        localStorage.setItem('lang', language);
+    },[language])
 
     return (
         <AppContext.Provider value={{ contentApplication, setLanguage, language, loadingThisContent,}}>
